d7f32c331f649b15575247ec0f77219137cf5cf8
bornkiller <hjj491229492@hotmail.com>
2015-01-07 17:26:35 +0800

    initial prototype coming out.

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,22 +1,13 @@
 var should = require('should');
+var gulp = require('gulp');
+var through = require('through-gulp');
 var utils = require('../utils/utils.js');
 
 describe('utils module', function () {
-  var StyleComment = '<!-- build:css /style/build.css -->'
-                   + '<link type="text/css" href="/style/origin.css">'
-                   + '<!-- endbuild -->';
-
-  var StyleMirrorComment = '<!-- build:css ./style/build.css -->'
-                         + '<link type="text/css" href="/style/origin.css">'
-                         + '<!-- endbuild -->';
-
-  var ScriptComment = '<!-- build:js /style/build.js -->'
-                    + '<script src="/script/origin.js></script>'
-                    + '<!-- endbuild -->';
-
-  var LessComment = '<!-- build:less /style/build.css -->'
-                  + '<link type="text/css" href="/style/origin.less">'
-                  + '<!-- endbuild -->';
+  var StyleComment = '<!-- build:css /style/build.css --><link type="text/css" href="/style/origin.css"><!-- endbuild -->';
+  var StyleMirrorComment = '<!-- build:css ./style/build.css --><link type="text/css" href="/style/origin.css"><!-- endbuild -->';
+  var ScriptComment = '<!-- build:js /style/build.js --><script src="/script/origin.js></script><!-- endbuild -->';
+  var LessComment = '<!-- build:less /style/build.css --><link type="text/css" href="/style/origin.less"><!-- endbuild -->';
 
   it('should get js type', function () {
     utils.getBlockType(StyleComment).should.equal('css');
@@ -36,5 +27,65 @@ describe('utils module', function () {
 
   it('should get relative destiny path', function () {
     utils.getBlockPath(StyleMirrorComment).should.equal('./style/build.css');
+  });
+
+  it('should split html into blocks', function (done) {
+    var expected = [
+      '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>gulp release</title>',
+      '<!-- build:css /style/build.css --><link rel="stylesheet" href="/style/origin.css"><link rel="stylesheet" href="/style/complex.css"><!-- endbuild -->',
+      '<!-- build:js /script/build.js --><script src="/script/origin.js"></script><script src="/script/complex.js"></script><!-- endbuild -->',
+      '</head><body></body></html>'
+    ];
+    gulp.src('./test/fixture/source.html')
+      .pipe(through(function(file, enc, callback) {
+        var result = utils.getSplitBlock(file.contents.toString());
+        (utils._escape(result[0])).should.equal(utils._escape(expected[0]));
+        (utils._escape(result[1])).should.equal(utils._escape(expected[1]));
+
+        (utils._escape(result[2])).should.equal(utils._escape(expected[2]));
+        (utils._escape(result[3])).should.equal(utils._escape(expected[3]));
+        callback();
+      }, function(callback) {
+        callback();
+        done();
+      }))
+  });
+
+  it.skip('should resolve file path from blocks', function (done) {
+    var ScriptPath = ['/script/origin.js', '/script/complex.js'];
+    var StylePath = ['/style/origin.css', '/script/complex.css'];
+    gulp.src('./test/fixture/source.html')
+      .pipe(through(function(file, enc, callback) {
+        var expected = [
+          '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>gulp release</title>',
+          '<!-- build:css /style/build.css --><link rel="stylesheet" href="/style/origin.css"><link rel="stylesheet" href="/style/complex.css"><!-- endbuild -->',
+          '<!-- build:js /style/build.js --><script src="/script/origin.js"></script><script src="/script/complex.js"></script><!-- endbuild -->',
+          '</head><body></body></html>'
+        ];
+        var result = utils.getSplitBlock(file.contents.toString());
+        callback();
+      }, function(callback) {
+        callback();
+        done();
+      }))
+  });
+
+  it('should resolve source into destiny', function (done) {
+    gulp.src('./test/fixture/source.html')
+      .pipe(through(function(file, enc, callback) {
+        var expected =
+          '<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>gulp release</title>' +
+          '<link rel="stylesheet" href="/style/build.css"/>' +
+          '<script src="/script/build.js"></script>' +
+          '</head><body></body></html>';
+
+        var blocks = utils.getSplitBlock(file.contents.toString());
+        var result = utils.resolveSourceToDestiny(blocks);
+        (utils._escape(result)).should.equal(utils._escape(expected));
+        callback();
+      }, function(callback) {
+        callback();
+        done();
+      }))
   });
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,19 +1,52 @@
-//var startReg = /<!--\s*build:(\w+)(?:(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))?)?\s*-->/gim;
-//var endReg = /<!--\s*endbuild\s*-->/gim;
-//var jsReg = /<\s*script\s+.*?src\s*=\s*("|')([^"']+?)\1.*?><\s*\/\s*script\s*>/gi;
-//var cssReg = /<\s*link\s+.*?href\s*=\s*("|')([^"']+)\1.*?>/gi;
-//var cssMediaReg = /<\s*link\s+.*?media\s*=\s*("|')([^"']+)\1.*?>/gi;
-
+var startReg = /<!--\s+build:\w+\s+\/?[^\s]+\s+-->/gim;
+var startMirrorReg = /<!--\s+build:\w+\s+\/?[^\s]+\s+-->/i;
+var endReg = /<!--\s*endbuild\s*-->/gim;
+var splitReg = /<!--\s+split\s+-->/gim;
+var jsReg = /<\s*script\s+.*?src\s*=\s*("|')([^"']+?)\1.*?><\s*\/\s*script\s*>/i;
+var cssReg = /<\s*link\s+.*?href\s*=\s*("|')([^"']+)\1.*?>/i;
+var spaceReg = /^\s*$/;
 var typeReg = /<!--\s+build:(\w+)\s+\/?[^\s]+\s+-->/i;
 var pathReg = /<!--\s+build:\w+\s+(\/?[^\s]+)\s+-->/i;
 var utils = {};
 
-utils.getBlockType = function(string) {
-  return typeReg.exec(string)[1];
+utils.getSplitBlock = function(string) {
+  return string.split(splitReg);
 };
 
-utils.getBlockPath = function(string) {
-  return pathReg.exec(string)[1];
+utils.getBlockType = function(block) {
+  return typeReg.exec(block)[1];
+};
+
+utils.getBlockPath = function(block) {
+  return pathReg.exec(block)[1];
+};
+
+utils.getFilePath = function(block) {
+  return block
+    .replace(startReg, '')
+    .replace(endReg, '')
+    .split('\n')
+    .filter(function(value) {
+      return !spaceReg.test(value);
+    })
+    .map(function(value) {
+      if (utils.getBlockType(block) === 'js') return jsReg.exec(value.replace(/^\s*/, ''))[2];
+      if (utils.getBlockType(block) === 'css') return cssReg.exec(value.replace(/^\s*/, ''))[2];
+    });
+};
+
+utils.resolveSourceToDestiny = function(blocks) {
+  var result = blocks.map(function(block) {
+    if (!startMirrorReg.test(block)) return block;
+    if (utils.getBlockType(block) === 'js') return '<script src="' + utils.getBlockPath(block) + '"></script>';
+    if (utils.getBlockType(block) === 'css') return '<link rel="stylesheet" href="' + utils.getBlockPath(block) + '"/>';
+  });
+
+  return result.join('\n');
+};
+
+utils._escape = function(string) {
+  return string.replace(/[\n\s]*/gi, '');
 };
 
 module.exports = utils;
