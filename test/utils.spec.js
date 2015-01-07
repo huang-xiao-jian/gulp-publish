f29fa595845cd709f84e7dafe8137d5433a0ad2d
bornkiller <hjj491229492@hotmail.com>
2015-01-07 11:56:53 +0800

    initial step test.

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,12 +1,40 @@
 var should = require('should');
 var utils = require('../utils/utils.js');
 
-var StyleComment = '<!-- build:css /style/build.css -->'
-                 + '<link type="text/css" href="/style/origin.css">'
-                 + '<!-- endbuild -->';
-var scriptComment = '<!-- build:js /style/build.js -->'
-                 + '<script src="/script/origin.js></script>'
-                 + '<!-- endbuild -->';
-var LessComment = '<!-- build:less /style/build.css -->'
-                 + '<link type="text/css" href="/style/origin.less">'
-                 + '<!-- endbuild -->';
+describe('utils module', function () {
+  var StyleComment = '<!-- build:css /style/build.css -->'
+                   + '<link type="text/css" href="/style/origin.css">'
+                   + '<!-- endbuild -->';
+
+  var StyleMirrorComment = '<!-- build:css ./style/build.css -->'
+                         + '<link type="text/css" href="/style/origin.css">'
+                         + '<!-- endbuild -->';
+
+  var ScriptComment = '<!-- build:js /style/build.js -->'
+                    + '<script src="/script/origin.js></script>'
+                    + '<!-- endbuild -->';
+
+  var LessComment = '<!-- build:less /style/build.css -->'
+                  + '<link type="text/css" href="/style/origin.less">'
+                  + '<!-- endbuild -->';
+
+  it('should get js type', function () {
+    utils.getBlockType(StyleComment).should.equal('css');
+  });
+
+  it('should get css type', function () {
+    utils.getBlockType(ScriptComment).should.equal('js');
+  });
+
+  it('should get other type', function () {
+    utils.getBlockType(LessComment).should.equal('less');
+  });
+
+  it('should get absolute destiny path', function () {
+    utils.getBlockPath(StyleComment).should.equal('/style/build.css');
+  });
+
+  it('should get relative destiny path', function () {
+    utils.getBlockPath(StyleMirrorComment).should.equal('./style/build.css');
+  });
+});
--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,19 @@
-var startReg = /<!--\s*build:(\w+)(?:(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))?)?\s*-->/gim;
-var endReg = /<!--\s*endbuild\s*-->/gim;
-var jsReg = /<\s*script\s+.*?src\s*=\s*("|')([^"']+?)\1.*?><\s*\/\s*script\s*>/gi;
-var cssReg = /<\s*link\s+.*?href\s*=\s*("|')([^"']+)\1.*?>/gi;
-var cssMediaReg = /<\s*link\s+.*?media\s*=\s*("|')([^"']+)\1.*?>/gi;
-var startCondReg = /<!--\[[^\]]+\]>/gim;
-var endCondReg = /<!\[endif\]-->/gim;
+//var startReg = /<!--\s*build:(\w+)(?:(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))?)?\s*-->/gim;
+//var endReg = /<!--\s*endbuild\s*-->/gim;
+//var jsReg = /<\s*script\s+.*?src\s*=\s*("|')([^"']+?)\1.*?><\s*\/\s*script\s*>/gi;
+//var cssReg = /<\s*link\s+.*?href\s*=\s*("|')([^"']+)\1.*?>/gi;
+//var cssMediaReg = /<\s*link\s+.*?media\s*=\s*("|')([^"']+)\1.*?>/gi;
 
+var typeReg = /<!--\s+build:(\w+)\s+\/?[^\s]+\s+-->/i;
+var pathReg = /<!--\s+build:\w+\s+(\/?[^\s]+)\s+-->/i;
 var utils = {};
 
-utils.getBlockType = function() {}
+utils.getBlockType = function(string) {
+  return typeReg.exec(string)[1];
+};
 
+utils.getBlockPath = function(string) {
+  return pathReg.exec(string)[1];
+};
+
+module.exports = utils;
